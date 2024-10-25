8ef4d8eef8aa6a12d5204e709f0f0e2f791e8786
eeturoponenn <eetu.roponen@gmail.com>
2024-10-25 18:50:26 +0300

    Add init_db function

--- App.py
+++ App.py
@@ -14,7 +14,7 @@ load_dotenv()  # This loads the variables from .env
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'instance', 'test.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -149,8 +149,9 @@ def register():
 
 migrate = Migrate(app, db)
 
-with app.app_context():
-    db.create_all()
+def init_db():
+    with app.app_context():
+        db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
